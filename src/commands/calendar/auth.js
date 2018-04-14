116f2967cfe5f5cc06657d085b654c898c824b82
Austin Reuland <amreuland@gmail.com>
2018-04-14 14:29:26 -0500

    Rename

--- src/commands/calendar/auth.js
+++ src/commands/calendar/auth.js
@@ -42,33 +42,6 @@ class AuthCalendar extends Command {
     await guild.authGoogle(authCode)
 
     return responder.send('Now call !calselect to select a calendar')
-
-    // return client.models.guild.findOne({ guildId })
-    //   .then(guildData => {
-    //     if (guildData && guildData.authToken) {
-    //       return responder.selection(['Yes', 'No'], {
-    //         title: 'Are you sure you want to change the auth ticket?'
-    //       })
-    //         .then(selection => {
-    //           if (selection === 'Yes') {
-    //             return client.gcal.getAuthClient()
-    //           } else {
-    //             return Promise.reject({code: 2, msg: 'cancelled'})
-    //           }
-    //         })
-    //     }
-    //     return client.gcal.getAuthClient()
-    //   })
-    //   .then(authClient => client.gcal.getAuthUrl(authClient)
-    //     .then(authUrl => responder.dialog([
-    //       {
-    //         prompt: `Authorize the bot by visiting this url: \n\n ${authUrl} \n\n Respond with the code from that page`,
-    //         input: { name: 'response', type: 'string', bot: false }
-    //       }])
-    //     )
-    //     .then(({response: code}) => client.gcal.getTokenFromCode(authClient, code))
-    //   )
-    //   .then(token => responder.send(token))
   }
 }
 
