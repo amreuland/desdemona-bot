ddede7d422aa7f1b4cf661fc3e9d0c498d160200
noriah <vix@noriah.dev>
2018-04-23 10:36:11 -0500

    Added: user join, user leave, username change

--- src/util/events.js
+++ src/util/events.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const moment = require('moment')
+
 const colors = {
   userJoin: 0x7ba600,
   userLeave: 0xd12b2b,
@@ -11,7 +13,9 @@ const colors = {
 
 const emojis = {
   nicknameChanged: ':capital_abcd:',
-  rolesChanged: ':card_index:'
+  rolesChanged: ':card_index:',
+  userJoin: ':inbox_tray:',
+  userLeave: ':outbox_tray:'
 }
 
 class EventUtils {
@@ -19,11 +23,51 @@ class EventUtils {
     EventUtils._client = client
   }
 
-  static createUsernameChangeEmbed (oldUser, newUser) {
+  static createUserJoinEmbed (guild, member) {
+    let description = `${emojis.userJoin} **${member.username}#${member.discriminator}** has \`joined\` the server. (${member.id})`
 
+    let userCreateDiff = moment().diff(member.createdAt, 'hours')
+
+    if (userCreateDiff < 24) {
+      description = description + `\n\n**Account created ${userCreateDiff} hours ago**`
+    }
+
+    return {
+      color: colors.userJoin,
+      description,
+      footer: {
+        text: `User Join (${guild.memberCount})`,
+        icon_url: member.avatarURL
+      },
+      timestamp: new Date()
+    }
   }
 
-  static createNickChangeEmbed (oldNick, newNick, avatarUrl) {
+  static createUserLeaveEmbed (guild, member) {
+    return {
+      color: colors.userLeave,
+      description: `${emojis.userLeave} **${member.username}#${member.discriminator}** has \`left\` the server. (${member.id})`,
+      footer: {
+        text: `User Leave (${guild.memberCount})`,
+        icon_url: member.avatarURL
+      },
+      timestamp: new Date()
+    }
+  }
+
+  static createUsernameChangedEmbed (oldUser, newUser) {
+    return {
+      color: colors.usernameChanged,
+      description: `${emojis.nicknameChanged} **${oldUser.username}#${oldUser.discriminator}** is now known as **${newUser.username}#${newUser.discriminator}**`,
+      footer: {
+        text: 'Username Changed',
+        icon_url: newUser.avatarURL
+      },
+      timestamp: new Date()
+    }
+  }
+
+  static createNickChangedEmbed (oldNick, newNick, avatarUrl) {
     return {
       color: colors.nicknameChanged,
       description: `${emojis.nicknameChanged} **${oldNick}**'s nickname was \`changed\` to **${newNick}**`,
