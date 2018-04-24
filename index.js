947366e689d2446e9043d93e10011ffa0b8b82bd
Ashley Reuland <amreuland@gmail.com>
2018-04-24 15:08:53 -0500

    Small Change?

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const chalk = require('chalk')
 const path = require('path')
 const moment = require('moment')
 const { Crystal, utils: { delay } } = require('sylphy')
+
 require('longjohn')
 
 const config = require('./config/config')
