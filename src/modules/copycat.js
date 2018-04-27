ca6aba1e2eb0210b15d9da4b36c8bc1fbe81cf37
noriah <vix@noriah.dev>
2018-04-26 19:29:42 -0500

    Working on copycat feature

--- src/modules/copycat.js
+++ src/modules/copycat.js
@@ -7,14 +7,41 @@ class CopycatModule extends Module {
     super(...args, {
       name: 'copycat:logic',
       events: {
-        'messageCreate': 'onMessageCreate'
+        messageCreate: 'onMessageCreate',
+        messageUpdate: 'onMessageUpdate'
       }
     })
   }
 
   onMessageCreate (message) {
-
+    // First check cache for channel id as key
+    // If found, take value and use as copy channel
+    // Check Cache for flag saying we have already checked guiild db in X time
+    // If flag, don't check db
+    // If no flag, check db, using guild id, look for copycat channels
+    // If any found, set all in the database
+    // Set flag in cache saying we have checked
+    // Set expiry on flag
+    // Take key for channel and get value of destination
+    // Send message formatted to look like quote to destination
+    // Store source message id and destination message id in cache as
+    //    source => destination
+    // Set expiry on source key
   }
+
+  onMessageUpdate (message, OldMessage) {
+    // Check cache for message id matching message id
+    // If found, get value of shadow-message in copy channel
+    // Update shadow-message with message content
+    // Set expiry on message id
+  }
+
+  // Clearing flag:
+  // When command runs
+  // If we add a copy channel
+  // Set in database, set in cache, set expiry and remove flag
+  // If we remove channel
+  // Remove entry from cache
 }
 
 module.exports = CopycatModule
--- src/navi.js
+++ src/navi.js
@@ -8,16 +8,17 @@ const chalk = require('chalk')
 const path = require('path')
 const winston = require('winston')
 const moment = require('moment')
+const redis = require('redis')
 
 const { Sentry } = require('./lib')
 
-const { APIPlugin, MongoosePlugin } = require('./plugins')
+const { APIPlugin, MongoosePlugin, RedisPlugin } = require('./plugins')
 
 const handleEvents = require('./lib/handleEvents')
 
 const {
   CleverbotAPI, GoogleAPI, LeagueAPI, OverwatchAPI,
-  PastebinAPI, SoundCloudAPI, SteamAPI
+  PastebinAPI, SoundCloudAPI
 } = require('./api')
 
 const { Client, utils } = require('sylphy')
@@ -50,6 +51,7 @@ class Navi extends Client {
 
     this
       .createPlugin('api', APIPlugin, options)
+      .createPlugin('cache', RedisPlugin, options)
       .createPlugin('db', MongoosePlugin, options)
 
     this
@@ -63,6 +65,10 @@ class Navi extends Client {
 
   get api () {
     return this.plugins.get('api')
+  }
+
+  get cache () {
+    return this.plugins.get('cache')
   }
 
   get db () {
@@ -111,12 +117,12 @@ const bot = new Navi({
   maxShards
 })
 
-const userBot = new Client({
-  token: config.user.token,
-  selfbot: true
-})
+// const userBot = new Client({
+//   token: config.user.token,
+//   selfbot: true
+// })
 
-bot.userBot = userBot
+// bot.userBot = userBot
 
 bot.register('api', 'google', GoogleAPI, require(resolveConfig('client_secret')).installed)
 bot.register('api', 'cleverbot', CleverbotAPI)
@@ -124,14 +130,18 @@ bot.register('api', 'lol', LeagueAPI)
 bot.register('api', 'overwatch', OverwatchAPI)
 bot.register('api', 'pastebin', PastebinAPI)
 bot.register('api', 'soundcloud', SoundCloudAPI)
-bot.register('api', 'steam', SteamAPI, config.steam.apiKey)
+// bot.register('api', 'steam', SteamAPI, config.steam.apiKey)
+
+const copycatCache = redis.createClient(config.cache.copycat)
+
+bot.register('cache', 'copycat', copycatCache)
 
 async function initStatusClock () {
   let index = 0
   const statuses = [
     'https://navi.social',
     '%s guilds',
-    'Use \'n!help\'',
+    `Use '${bot.prefix}help'`,
     '%d users'
   ]
   setInterval(function () {
@@ -151,4 +161,5 @@ bot.once('ready', () => {
   setInterval(handleEvents.bind(bot), (config.calendar.pollingRate || 30) * 1000)
 })
 
-bot.run().then(() => userBot.run())
+bot.run()
+// .then(() => userBot.run())
