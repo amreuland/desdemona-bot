4f0852fccbd972502debbafda60547ffbdd61697
Austin Reuland <amreuland@gmail.com>
2018-04-27 20:54:21 -0500

    Working copycat

--- src/util/copycat.js
+++ src/util/copycat.js
@@ -1,21 +1,32 @@
 'use strict'
 
+const moment = require('moment')
+
 class CopycatUtils {
-
   static createMirrorEmbed (msg) {
-    let description = msg.content
-    let title =
-    if (msg.member.nick) {
-
+    let description = `**Posted in ${msg.channel.mention}**\n\n${msg.content}\n\n**End Message**`
+    let author = {
+      name: `${msg.author.username}#${msg.author.discriminator}`,
+      icon_url: msg.author.avatarURL
     }
+
+    let title = `Posted in ${msg.channel.mention}`
+
+    let footer = {
+      text: `User ID: ${msg.author.id}`
+    }
+
+    let timestamp = moment(msg.timestamp).toDate()
+
+    let color = 0xff8300
+
     return {
-      content: msg.content,
-      nick: msg.member.nick,
-      username: `${msg.author.username}#${msg.author.discriminator}`
-      userId: msg.author.
+      description,
+      timestamp,
+      color,
+      footer,
+      author
     }
-
-    return
   }
 }
 
