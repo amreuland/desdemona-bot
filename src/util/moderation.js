42f748e565acbf720e91fa469a5240b17791fbe9
noriah <vix@noriah.dev>
2018-04-30 16:21:41 -0500

    Added Warnlog

--- src/util/moderation.js
+++ src/util/moderation.js
@@ -1,6 +1,52 @@
 'use strict'
 
+const moment = require('moment')
+
+const warningFormat = ts => moment(ts).format('ddd, MMM DD YYYY, HH:mm')
+
 class ModerationUtils {
+  static createWarnlogEmbed (member, warnings, owc) {
+    let fields = []
+
+    let idx = 0
+    warnings.forEach(warning => {
+      idx++
+      fields.push({
+        name: `#${idx} on ${warningFormat(warning.timestamp)}${warning.forgiven ? ' (forgiven)' : ''}`,
+        value: warning.reason
+      })
+    })
+
+    let name = `${member.username}#${member.discriminator}`
+
+    let description = [
+      `**${name}** joined on **${warningFormat(member.joinedAt)}**`,
+      `They have **${warnings.length}** total warning${warnings.length > 1 ? 's' : ''} on this server.`
+    ]
+
+    if (owc > 0) {
+      description.push(`In addition, they have ${owc} unforgiven warning${owc > 1 ? 's' : ''} in other servers.`)
+    }
+
+    description = description.join('\n')
+
+    return {
+      title: `Warning log for ${name}`,
+      description,
+      thumbnail: {
+        url: member.avatarURL,
+        width: 128,
+        height: 128
+      },
+      fields,
+      footer: {
+        text: 'Warning Log'
+      },
+      timestamp: new Date(),
+      color: 0xffcc00
+    }
+  }
+
   static createWarningEmbed (guild, moderator, warnCount, reason) {
     let fields = [
       {
