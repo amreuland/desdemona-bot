f9b7c8e7c0c241f749b9b2f48373c813e9b77156
noriah <vix@noriah.dev>
2018-05-07 20:58:42 -0500

    Add ban and kick commands

---
+++ src/modules/moderation/commands/ban.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const R = require('ramda')
+
+const { Command } = require('sylphy')
+
+const { ActionCanceledException } = require('../errors')
+
+const ModerationUtils = require('../util')
+
+class BanCommand extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'ban',
+      description: 'Ban a user after sending them a message',
+      usage: [
+        { name: 'member', displayName: 'member', type: 'member' },
+        { name: 'reason', displayName: 'reason', type: 'string', optional: true, last: true }
+      ],
+      options: {
+        guildOnly: true,
+        permissions: ['banMembers']
+      },
+      examples: [
+        {
+          args: '@Apples Multiple Warnings',
+          description: 'Ban the user Apples with the reason \'Multiple Warnings\''
+        }
+      ]
+    })
+  }
+
+  async handle ({ msg, client, args }, responder) {
+    let guild = msg.channel.guild
+    let guildId = guild.id
+    let member = args.member[0]
+    let userId = member.id
+    let reason = args.reason || 'No Reason Provided'
+
+
+    return msg.delete('Hide moderation commands')
+      .then(() => {
+        return responder.selection(['Yes', 'No'], `Are you sure you want to ban ${member.mention}`)
+          .then(response => {
+            if (response[0] !== 'Yes') {
+              return Promise.reject(new ActionCanceledException())
+            }
+          })
+      })
+      .then(() => client.db.User.findOne({ userId }))
+      .then(dbUser => {
+        if (!dbUser) {
+          return client.db.User.create({ userId })
+        }
+
+        return dbUser
+      })
+      .then(dbUser => {
+        return client.db.Warning.create({
+          user: dbUser._id,
+          userId,
+          guildId,
+          reason: `**USER BANNED**\n${reason}`,
+          timestamp: new Date(),
+          moderatorId: msg.author.id
+        })
+      })
+      .then(() => member.user.getDMChannel())
+      .then(dmChannel => {
+        let embed = ModerationUtils.createBanMsgEmbed(guild, reason)
+        return dmChannel.createMessage({ embed })
+      })
+      .then(() => {
+        return member.ban(0, reason)
+      })
+      .then(() => {
+        return responder.success('{{ban.SUCCESS}}', {
+          deleteDelay: 5,
+          member: member.mention
+        })
+      })
+      .catch(ActionCanceledException, () => {
+        return responder.success('Action canceled')
+      })
+  }
+}
+
+module.exports = BanCommand
---
+++ src/modules/moderation/commands/kick.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const R = require('ramda')
+
+const { Command } = require('sylphy')
+
+const ModerationUtils = require('../util')
+
+class KickCommand extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'kick',
+      description: 'Kick a user after sending them a message',
+      usage: [
+        { name: 'member', displayName: 'member', type: 'member' },
+        { name: 'reason', displayName: 'reason', type: 'string', optional: true, last: true }
+      ],
+      options: {
+        guildOnly: true,
+        permissions: ['kickMembers']
+      },
+      examples: [
+        {
+          args: '@Apples Multiple Warnings',
+          description: 'Kick the user Apples with the reason \'Multiple Warnings\''
+        }
+      ]
+    })
+  }
+
+  async handle ({ msg, client, args }, responder) {
+    let guild = msg.channel.guild
+    let guildId = guild.id
+    let member = args.member[0]
+    let userId = member.id
+    let reason = args.reason || 'No Reason Provided'
+
+
+    return msg.delete('Hide moderation commands')
+      .then(() => {
+        return responder.selection(['Yes', 'No'], `Are you sure you want to kick ${member.mention}`)
+          .then(response => {
+            if (response[0] !== 'Yes') {
+              return Promise.reject(new ActionCanceledException())
+            }
+          })
+      })
+      .then(() => client.db.User.findOne({ userId }))
+      .then(dbUser => {
+        if (!dbUser) {
+          return client.db.User.create({ userId })
+        }
+
+        return dbUser
+      })
+      .then(dbUser => {
+        return client.db.Warning.create({
+          user: dbUser._id,
+          userId,
+          guildId,
+          reason: `**USER KICKED**\n${reason}`,
+          timestamp: new Date(),
+          moderatorId: msg.author.id
+        })
+      })
+      .then(() => member.user.getDMChannel())
+      .then(dmChannel => {
+        let embed = ModerationUtils.createKickMsgEmbed(guild, reason)
+        return dmChannel.createMessage({ embed })
+      })
+      .then(() => {
+        return member.kick(reason)
+      })
+      .then(() => {
+        return responder.success('{{kick.SUCCESS}}', {
+          deleteDelay: 5,
+          member: member.mention
+        })
+      })
+  }
+}
+
+module.exports = KickCommand
